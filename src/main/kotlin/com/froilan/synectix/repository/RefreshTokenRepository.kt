0344e7b0516a3ced002b5f3ef722c3c8e437f07a
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-03-29 16:33:45 +0800

    Add @Repository annotation to RefreshTokenRepository for consistency

--- src/main/kotlin/com/froilan/synectix/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/froilan/synectix/repository/RefreshTokenRepository.kt
@@ -2,8 +2,10 @@ package com.froilan.synectix.repository
 
 import com.froilan.synectix.model.RefreshToken
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 import java.util.Optional
 
+@Repository
 interface RefreshTokenRepository : MongoRepository<RefreshToken, String> {
     fun findByToken(token: String): Optional<RefreshToken>
     fun deleteByToken(token: String)
