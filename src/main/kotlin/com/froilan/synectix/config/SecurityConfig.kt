7b272ffc433c17ffca68234ff28ddc8ad629b178
Froilan Aquino <61849282+aquinofroilan@users.noreply.github.com>
2026-04-03 17:23:43 +0800

    Fix import ordering in SecurityConfig

--- src/main/kotlin/com/froilan/synectix/config/SecurityConfig.kt
+++ src/main/kotlin/com/froilan/synectix/config/SecurityConfig.kt
@@ -4,10 +4,10 @@ import com.froilan.synectix.security.SynectixPermissionEvaluator
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
